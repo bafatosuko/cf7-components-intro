040911c645670d9f9fdaa56c098da6e6258f7a45
Th. Konstantinou <bafatosuko@gmail.com>
2025-05-22 09:41:35 +0300

    feat: tailwindcss implementation

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,7 @@
+const ArrowFunctionalComponent  = () => {
+    const title: string = "Is a Arrow Fuctional Component";
+    return <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>
+
+}
+
+export default ArrowFunctionalComponent;

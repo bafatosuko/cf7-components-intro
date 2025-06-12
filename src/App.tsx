de32f462de7c2d14236b168798ad62b38f4089ad
Th. Konstantinou <bafatosuko@gmail.com>
2025-06-12 18:43:59 +0300

    feat : add MultiFieldFormWithValidation. component

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,8 @@ import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 // import FocusInput from "./components/FocusInput.tsx";
 // import UncontrolledInput from "./components/UncontrolledInput.tsx";
-import MultiFieldForm from "./components/MultiFieldForm.tsx";
+// import MultiFieldForm from "./components/MultiFieldForm.tsx";
+import MultiFieldFormWithValidation from "./components/MultiFieldFormWithValidation.tsx";
 
 
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
@@ -73,7 +74,7 @@ function App() {
             {/*<Route path="/" element={<HomePage/>} />*/}
             <Route element={<RouterLayout/>}>
               {/*<Route index element={<FocusInput/>}/>*/}
-              <Route index element={<MultiFieldForm/>}/>
+              <Route index element={<MultiFieldFormWithValidation/>}/>
               {/*<Route index element={<UncontrolledInput/>}/>*/}
               {/*<Route index element={<HomePage/>} />*/}
             </Route>
